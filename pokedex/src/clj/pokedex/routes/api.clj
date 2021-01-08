4013e0ee6b2995c7e3568e8c6c224681e03f354a
Ryan Done <rbdone@gmail.com>
2021-01-08 15:42:39 -0700

    Inline function example

--- pokedex/src/clj/pokedex/routes/api.clj
+++ pokedex/src/clj/pokedex/routes/api.clj
@@ -1,17 +1,23 @@
 (ns pokedex.routes.api
   (:require
-   [pokedex.layout :as layout]
-   [clojure.java.io :as io]
-   [pokedex.middleware :as middleware]
-   [ring.util.response]
-   [ring.util.http-response :as response]))
+    [pokedex.layout :as layout]
+    [clojure.java.io :as io]
+    [pokedex.middleware :as middleware]
+    [ring.util.response]
+    [ring.util.http-response :as response]))
 
 (defn api-page [request]
   (layout/render request "api.html"))
+
+(defn pokedex [request])
 
 (defn api-routes []
   ["/api"
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
-   ["/" {:get api-page}]])
+   ["/" {:get api-page}]
+   ["/pokedex" {:get (fn [request]
+                       {:status  200
+                        :headers {}
+                        :body    {:foo "Bar"}})}]])
 
