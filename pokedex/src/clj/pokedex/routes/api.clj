41e983df1649d2a50cf4037da692c0406eff55eb
Ryan Done <rbdone@gmail.com>
2021-01-11 10:14:43 -0700

    Format data with pokemon name as key

--- pokedex/src/clj/pokedex/routes/api.clj
+++ pokedex/src/clj/pokedex/routes/api.clj
@@ -7,13 +7,51 @@
     [ring.util.http-response :as response]
     [jsonista.core :as j]))
 
+
+
 (defn api-page [request]
   (layout/render request "api.html"))
 
+
+
+(defn convert-item [item]
+  (def itemName (get (get item "name") "english"))
+  (def itemBase (get item "base"))
+  (def itemType (get item "type"))
+
+  {(keyword `~itemName) {:id   (get item "id")
+                         :name itemName
+                         :base itemBase
+                         :type itemType}})
+
+
+(defn convert-to-hashmap [data]
+  (def firstItem (convert-item (first data)))
+  (println (count data))
+  (println firstItem)
+  (println "")
+
+  (if (= (count data) 1)
+    firstItem
+    (conj firstItem
+          (convert-to-hashmap (rest data)))))
+
+
+
 (defn pokedex [request]
+  (def file (java.io.File. "pokedex.json"))
+  (slurp file)
+  (def data (j/read-value file))
+
+  (println (convert-to-hashmap data))
+
   {:status  200
    :headers {"Content-Type" "application/json"}
-   :body    (j/write-value-as-bytes {:foo "bar" :age 23})})
+   :body    "test"})
+   ;:body    (j/write-value-as-bytes (convert-item combined))})
+   ;:body (j/write-value-as-bytes (convert-to-obj-map data))})
+
+
 
 (defn api-routes []
   ["/api"
